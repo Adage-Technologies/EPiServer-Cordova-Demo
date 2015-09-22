744bcc8aa48006f87896786a383a78d13560ba86
calvin belden <cbelden@adagetechnologies.com>
2015-09-22 12:31:45 -0500

    Initial commit

---
+++ episerver-cordova/platforms/android/assets/www/scripts/index.js
@@ -0,0 +1,72 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function (angular, Q, _) {
+    "use strict";
+
+    /**
+     * Application constants
+     */
+    var CONTENT_API_URL = '';
+    var HOME_PAGE_ID = 'home';
+
+
+    /**
+     * Angular application declaration
+     */
+    angular.module('HelloWorldApp', [])
+        .factory('contentService', contentServiceFactory)
+        .factory('contentApi', contentApiFactory)
+        .controller('helloWorldController', helloWorldController);
+
+
+    
+
+    /**
+     * Exposes our app's content for rendering.
+     */
+    helloWorldController.$inject = ['$scope', 'contentService', 'myAppService'];
+    function helloWorldController($scope, contentService, spinner) {
+        var _this = this;
+        _this.page = {};
+
+        spinner.wait();
+
+        // Attempt to retrieve the home page's content
+        contentService.get()
+
+            // Merge page properties with the controller
+            .then(function (content) {
+                $scope.$applyAsync(function () {
+                    _.assign(_this.page, content);
+                });
+            })
+
+            // Handle an error (i.e. from being offline and not having cached content)
+            .fail(function (error) {
+                $scope.$applyAsync(function () {
+                    console.log(error);
+                    _this.error = error;
+                });
+            });
+    }
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )(angular, Q, _);
---
+++ episerver-cordova/www/app/services.js
@@ -0,0 +1,125 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("myapp.services", [])
+        .factory("myappService", ["$rootScope", "$http", function ($rootScope, $http) {
+            var myappService = {};
+
+            //starts and stops the application waiting indicator
+            myappService.wait = function (show) {
+                if (show)
+                    $(".spinner").show();
+                else
+                    $(".spinner").hide();
+            };
+
+            return myappService;
+        }])
+        .factory('contentApi', contentApiFactory)
+        .factory('contentService', contentServiceFactory);
+
+
+    /**
+     * Requests content from the EPiServer backend
+     */
+    contentApiFactory.$inject = ['contentServicesConfig', '$http'];
+    function contentApiFactory(config, $http) {
+        return {
+            get: get,
+        };
+
+        /**
+         * Returns a promise that resolves content for the given pageId
+         */
+        function get(pageId) {
+            // When this is first called, the pageId is not known. Assume we are on the home page.
+            if (typeof pageId === 'undefined') {
+                return entryPoint()
+                    .then(function (response) { return response.homePageId })
+                    .then(content);
+            }
+
+            return content(pageId);
+        }
+
+        /**
+         * Returns a promise that resolves the page id of the home page (ie the application 'entry point')
+         */
+        function entryPoint() {
+            var deferred = Q.defer();
+
+            // TODO : actually get the page id from the EPiServer backend
+            var pageId = config.HOME_PAGE_ID;
+            deferred.resolve({ homePageId: pageId });
+            return deferred.promise;
+        }
+
+        /**
+         * Returns a promise that resolves content for the given pageId
+         */
+        function content(pageId) {
+            var deferred = Q.defer();
+
+            // TODO : actually request the content from the EPiServer backend
+            var page = {
+                content: {
+                    whatText: "This is going to be CMS content that can be specified within an EPiServer site.",
+                    whoText: "This is going to be CMS content that can be specified within an EPiServer site.",
+                    whyText: "This is going to be CMS content that can be specified within an EPiServer site.",
+                    contactText: "This is going to be CMS content that can be specified within an EPiServer site.",
+                },
+                pageId: pageId
+            };
+
+            deferred.resolve(page);
+            return deferred.promise;
+        }
+    }
+
+
+    /**
+     * Retrieves all content.
+     */
+    contentServiceFactory.$inject = ['contentServicesConfig', 'contentApi'];
+    function contentServiceFactory(config, contentApi) {
+        return {
+            get: get
+        };
+
+        /**
+         * Will first attempt to retrieve the content from EPiServer. If this fails, we will check
+         * localStorage. If both fail, the returned promise will be rejected.
+         */
+        function get(pageId) {
+            return contentApi.get(pageId)
+                .catch(getFromLocalStorage)
+                .then(saveToLocalStorage)
+                .then(function (page) { return page.content });
+        }
+
+        function getFromLocalStorage(pageId) {
+            if (typeof pageId === 'undefined') {
+                pageId = config.HOME_PAGE_ID;
+            }
+
+            var content = localStorage.getItem('content' + pageId.toString());
+
+            if (content) {
+                return content;
+            }
+
+            throw new Error('No content found in local storage');
+        }
+
+        function saveToLocalStorage(page) {
+            // Do not store 'undefined'
+            if (typeof page === 'undefined') {
+                throw new Error('Attempted to save undefined.');
+            }
+
+            localStorage.setItem('content' + page.pageId.toString(), page);
+
+            return page;
+        }
+    }
+})();
